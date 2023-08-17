de96f7f12b110176f59c6377619510663c4e77b6
Airel Ribeiro <98190806+AirelRibeiro@users.noreply.github.com>
2023-08-17 20:11:42 -0300

    docs: inclui documentação da classe Laboratórios

--- Farmacia/Laboratorio.py
+++ Farmacia/Laboratorio.py
@@ -1,50 +1,95 @@
-class Laboratorio:
-    laboratorios_cadastrados = []
+class Cliente:
+    """
+    Classe que representa um cliente cadastrado.
 
-    def __init__(self, nome, endereco, telefone, cidade, estado):
+    Atributos:
+        cpf (str): CPF do cliente.
+        nome (str): Nome do cliente.
+        data_nascimento (str): Data de nascimento do cliente no formato 'dd/mm/aaaa'.
+    """
+
+    clientes_cadastrados = []
+
+    def __init__(self, cpf: str, nome: str, data_nascimento: str):
+        """
+        Inicializa uma instância de Cliente.
+
+        :param cpf: CPF do cliente (str).
+        :param nome: Nome do cliente (str).
+        :param data_nascimento: Data de nascimento do cliente no formato 'dd/mm/aaaa' (str).
+        """
+        self.__cpf = cpf
         self.__nome = nome
-        self.__endereco = endereco
-        self.__telefone = telefone
-        self.__cidade = cidade
-        self.__estado = estado
-        self.salvar_lab(self)
+        self.__data_nascimento = data_nascimento
+        self.salvar_cliente(self)
     
     @classmethod
-    def salvar_laboratorio(cls, lab):
-        cls.laboratorios_cadastrados.append(lab)
+    def salvar_cliente(cls, cliente: "Cliente"):
+        """
+        Adiciona um cliente à lista de clientes cadastrados.
 
-    def get_nome(self):
+        :param cliente: Instância de Cliente a ser cadastrada.
+        """
+        cls.clientes_cadastrados.append(cliente)
+
+    @staticmethod
+    def valida_cpf(cpf: str):
+        """
+        Valida um CPF.
+
+        :param cpf: CPF a ser validado (str).
+        """
+        pass  # Implementação da validação do CPF
+
+    def get_cpf(self) -> str:
+        """
+        Obtém o CPF do cliente.
+
+        :return: CPF do cliente (str).
+        """
+        return self.__cpf
+
+    def set_cpf(self, cpf: str):
+        """
+        Define o CPF do cliente, se o CPF for válido.
+
+        :param cpf: Novo CPF do cliente (str).
+        """
+        if self.valida_cpf(cpf):
+            self.__cpf = cpf
+
+    def get_nome(self) -> str:
+        """
+        Obtém o nome do cliente.
+
+        :return: Nome do cliente (str).
+        """
         return self.__nome
 
-    def set_nome(self, nome):
+    def set_nome(self, nome: str):
+        """
+        Define o nome do cliente.
+
+        :param nome: Novo nome do cliente (str).
+        """
         self.__nome = nome
 
-    def get_endereco(self):
-        return self.__endereco
+    def get_data_nascimento(self) -> str:
+        """
+        Obtém a data de nascimento do cliente.
 
-    def set_endereco(self, endereco):
-        self.__endereco = endereco
+        :return: Data de nascimento do cliente no formato 'dd/mm/aaaa' (str).
+        """
+        return self.__data_nascimento
 
-    def get_telefone(self):
-        return self.__telefone
+    def set_data_nascimento(self, data_nascimento: str):
+        """
+        Define a data de nascimento do cliente.
 
-    def set_telefone(self, telefone):
-        self.__telefone = telefone
+        :param data_nascimento: Nova data de nascimento do cliente no formato 'dd/mm/aaaa' (str).
+        """
+        self.__data_nascimento = data_nascimento
 
-    def get_cidade(self):
-        return self.__cidade
-
-    def set_cidade(self, cidade):
-        self.__cidade = cidade
-
-    def get_estado(self):
-        return self.__estado
-
-    def set_estado(self, estado):
-        self.__estado = estado
-
+    cpf = property(get_cpf, set_cpf)
     nome = property(get_nome, set_nome)
-    endereco = property(get_endereco, set_endereco)
-    telefone = property(get_telefone, set_telefone)
-    cidade = property(get_cidade, set_cidade)
-    estado = property(get_estado, set_estado)
+    data_nascimento = property(get_data_nascimento, set_data_nascimento)
