20991a280435338c923dbfb78f2468487ad2d47a
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2023-08-18 16:37:00 -0300

    fix: implementa classe laboratório
    -- Em algum momento colei a classe Cliente no arquivo Laboratorio.py

--- Farmacia/Laboratorio.py
+++ Farmacia/Laboratorio.py
@@ -1,98 +1,127 @@
-class Cliente:
+class Laboratorio:
     """
-    Classe que representa um cliente cadastrado.
+    Classe que representa um laboratório cadastrado.
 
     Atributos:
-        cpf (str): CPF do cliente.
-        nome (str): Nome do cliente.
-        data_nascimento (str): Data de nascimento do cliente
-            no formato 'dd/mm/aaaa'.
+        nome (str): Nome do laboratório.
+        endereco (str): Endereço (rua e bairro) do laboratório.
+        telefone (str): Número de telefone do laboratório.
+        cidade (str): Cidade do laboratório.
+        estado (str): Estado do laboratório.
     """
 
-    clientes_cadastrados = []
+    laboratorios_cadastrados = []
 
-    def __init__(self, cpf: str, nome: str, data_nascimento: str):
+    def __init__(
+        self, nome: str, endereco: str, telefone: str, cidade: str, estado: str
+    ):
         """
-        Inicializa uma instância de Cliente.
+        Inicializa uma instância de Laboratorio.
 
-        :param cpf: CPF do cliente (str).
-        :param nome: Nome do cliente (str).
-        :param data_nascimento: Data de nascimento do cliente
-            no formato 'dd/mm/aaaa' (str).
+        :param nome: str.
+        :param endereco: str.
+        :param telefone: str.
+        :param cidade: str.
+        :param estado: str.
         """
-        self.__cpf = cpf
         self.__nome = nome
-        self.__data_nascimento = data_nascimento
-        self.salvar_cliente(self)
+        self.__endereco = endereco
+        self.__telefone = telefone
+        self.__cidade = cidade
+        self.__estado = estado
+        self.salvar_laboratorio(self)
 
     @classmethod
-    def salvar_cliente(cls, cliente: "Cliente"):
+    def salvar_laboratorio(cls, lab: "Laboratorio") -> None:
         """
-        Adiciona um cliente à lista de clientes cadastrados.
+        Adiciona um laboratório à lista de laboratórios cadastrados.
 
-        :param cliente: Instância de Cliente a ser cadastrada.
+        :param lab: Instância de Laboratorio a ser cadastrada.
         """
-        cls.clientes_cadastrados.append(cliente)
-
-    @staticmethod
-    def valida_cpf(cpf: str):
-        """
-        Valida um CPF.
-
-        :param cpf: CPF a ser validado (str).
-        """
-        pass  # Implementação da validação do CPF
-
-    def get_cpf(self) -> str:
-        """
-        Obtém o CPF do cliente.
-
-        :return: CPF do cliente (str).
-        """
-        return self.__cpf
-
-    def set_cpf(self, cpf: str):
-        """
-        Define o CPF do cliente, se o CPF for válido.
-
-        :param cpf: Novo CPF do cliente (str).
-        """
-        if self.valida_cpf(cpf):
-            self.__cpf = cpf
+        cls.laboratorios_cadastrados.append(lab)
 
     def get_nome(self) -> str:
         """
-        Obtém o nome do cliente.
+        Obtém o nome do laboratório.
 
-        :return: Nome do cliente (str).
+        :return: Nome do laboratório (str).
         """
         return self.__nome
 
-    def set_nome(self, nome: str):
+    def set_nome(self, nome: str) -> None:
         """
-        Define o nome do cliente.
+        Define o nome do laboratório.
 
-        :param nome: Novo nome do cliente (str).
+        :param nome: Novo nome do laboratório (str).
         """
         self.__nome = nome
 
-    def get_data_nascimento(self) -> str:
+    def get_endereco(self) -> str:
         """
-        Obtém a data de nascimento do cliente.
+        Obtém o endereço do laboratório.
 
-        :return: Data de nascimento do cliente no formato 'dd/mm/aaaa' (str).
+        :return: Endereço do laboratório (str).
         """
-        return self.__data_nascimento
+        return self.__endereco
 
-    def set_data_nascimento(self, data_nascimento: str):
+    def set_endereco(self, endereco: str) -> None:
         """
-        Define a data de nascimento do cliente.
+        Define o endereço do laboratório.
 
-        :param data_nascimento: Nova data de nascimento do cliente
-            no formato 'dd/mm/aaaa' (str).
+        :param endereco: Novo endereço do laboratório (str).
         """
-        self.__data_nascimento = data_nascimento
+        self.__endereco = endereco
 
-    cpf = property(get_cpf, set_cpf)
+    def get_telefone(self) -> str:
+        """
+        Obtém o número de telefone do laboratório.
+
+        :return: Número de telefone do laboratório (str).
+        """
+        return self.__telefone
+
+    def set_telefone(self, telefone: str) -> None:
+        """
+        Define o número de telefone do laboratório.
+
+        :param telefone: Novo número de telefone do laboratório (str).
+        """
+        self.__telefone = telefone
+
+    def get_cidade(self) -> str:
+        """
+        Obtém a cidade do laboratório.
+
+        :return: Cidade do laboratório (str).
+        """
+        return self.__cidade
+
+    def set_cidade(self, cidade: str) -> None:
+        """
+        Define a cidade do laboratório.
+
+        :param cidade: Nova cidade do laboratório (str).
+        """
+        self.__cidade = cidade
+
+    def get_estado(self) -> str:
+        """
+        Obtém o estado do laboratório.
+
+        :return: Estado do laboratório (str).
+        """
+        return self.__estado
+
+    def set_estado(self, estado: str) -> None:
+        """
+        Define o estado do laboratório.
+
+        :param estado: Novo estado do laboratório (str).
+        """
+        self.__estado = estado
+
     nome = property(get_nome, set_nome)
-    data_nascimento = property(get_data_nascimento, set_data_nascimento)
+    endereco = property(get_endereco, set_endereco)
+    telefone = property(get_telefone, set_telefone)
+    cidade = property(get_cidade, set_cidade)
+    estado = property(get_estado, set_estado)
